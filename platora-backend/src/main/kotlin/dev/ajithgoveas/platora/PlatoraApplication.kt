dd7d54247e1db1ffeccb5918814747fae973e89b
Ajith Goveas <ajithgoveas@gmail.com>
2025-08-22 20:02:16 +0530

    Initial commit with Backend Code

---
+++ platora-backend/src/main/kotlin/dev/ajithgoveas/platora/PlatoraApplication.kt
@@ -0,0 +1,11 @@
+package dev.ajithgoveas.platora
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlatoraApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlatoraApplication>(*args)
+}
---
+++ platora-backend/src/test/kotlin/dev/ajithgoveas/platora/PlatoraApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.ajithgoveas.platora
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatoraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
